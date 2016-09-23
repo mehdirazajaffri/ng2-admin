594e7194c32a5730c354a99ec6f0eba9d06549c0
Baqir <muhammadbaqiraltaf@gmail.com>
2016-09-23 11:25:14 +0500

    some changes , created custom component

---
+++ src/app/pages/custom/custom.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {Route} from "@angular/router";
+
+@Component({
+  selector: 'new',
+  pipes: [],
+  providers: [],
+  styles: [],
+  template: ''
+})
+export class Custom {
+
+  constructor() {
+  }
+}
